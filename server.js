9c5842929b6c5c1f59b6a2404060759436fc0795
sbowman96 <sbowman96@gmail.com>
2017-05-29 18:16:01 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+
+app.use ((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log +'\n', (err) => {
+if (err) {
+  consol.log('unable to append log')
+}
+});
+  next();
+// });
+// app.use((req, res, next) => {
+//   res.render ("maint.hbs");
+});
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear()
+});
+
+app.get ('/' ,(req, res) => {
+    res.render('home.hbs',{
+    pageTitle: 'Home B Page',
+    welcomeMessage: "Welcome to the interactive website",
+
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "ABooout Page",
+    });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to help'
+});
+
+});
+
+app.listen(3000, () => {
+  console.log('Port is ready on 3k');
+});
